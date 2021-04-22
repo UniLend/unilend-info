03828c662e3f8fe2b434679cbb56608ddd03e54e
Thanush Praveen <thanushpraveen@aequalisys.com>
2021-04-22 11:35:16 +0530

    Updates

---
+++ precache-manifest.f2f61c64ae6b5414e0a8346000d5ff29.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b4747eab99f1dc67e942f295d45212",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3210b062cbf97fafc447",
+    "url": "/static/js/2.63a8e366.chunk.js"
+  },
+  {
+    "revision": "90c0b6da4a9af2bf8e5ab0961c02b768",
+    "url": "/static/js/2.63a8e366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "676c6f984c9bb0948716",
+    "url": "/static/js/main.fdd8322a.chunk.js"
+  },
+  {
+    "revision": "db6a2031b947dd515d1a",
+    "url": "/static/js/runtime-main.6df433cf.js"
+  },
+  {
+    "revision": "5fc0c9bdb40040928ed73dbbcecd62a3",
+    "url": "/static/media/eth.5fc0c9bd.png"
+  },
+  {
+    "revision": "5827780d34d7c141d5e763fd7b596313",
+    "url": "/static/media/logo.5827780d.svg"
+  },
+  {
+    "revision": "401c0644d6c2c4a509c5f70088d684cb",
+    "url": "/static/media/logo.unilend.401c0644.svg"
+  },
+  {
+    "revision": "edb44e56409a7af705c227bd272fac47",
+    "url": "/static/media/logo_white.edb44e56.svg"
+  },
+  {
+    "revision": "9914390fc6b153e39de552594e0b1d3a",
+    "url": "/static/media/wordmark_white.9914390f.svg"
+  }
+]);
